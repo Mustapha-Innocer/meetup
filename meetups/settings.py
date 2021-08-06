e62f66b129974d496115fbd08eda3bac2c197fa3
Mustapha Innocer <mustaphainnocer@gmail.com>
2021-08-06 22:31:44 +0000

    added the requirement.txt file

--- meetups/settings.py
+++ meetups/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-17ck$z&pbhnl&9c#*3yoy*!j^9p$-u@vykz+h$)qz38ye(n*0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['minnocer-meetup.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
